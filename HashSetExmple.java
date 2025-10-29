84120701f161241a62b3639eacecf930e0e8c727
nique-e <niteshmule023@gmail.com>
2025-10-29 23:54:32 +0530

    Linked hash set changes

--- HashSetExmple.java
+++ HashSetExmple.java
@@ -1,14 +1,12 @@
 
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 
 public class HashSetExmple{
     public static void main(String[] args) {
         
         HashSet hs = new HashSet();
         hs.add("Nitesh");
-        hs.add(23);
-        hs.add(23);
-        hs.add(23);
         hs.add(23);
         hs.add(56.1);
 
@@ -22,6 +20,21 @@ public class HashSetExmple{
 
          hs.clear();
          System.out.println("after clear method hashset is :" +hs);
+
+         HashSet hs2 = new HashSet();
+         hs2.add(10);
+         hs2.add(20);
+
+         hs2.addAll(hs);
+         System.out.println("after add all :" +hs2);
+
+         LinkedHashSet lhs = new LinkedHashSet();
+         
+         lhs.add("anbs");
+         lhs.add(20);
+         lhs.add(56.3);
+
+
 
     }
 }

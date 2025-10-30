9c615cd2c9523769118922885159fd81997052db
nique-e <niteshmule023@gmail.com>
2025-10-30 15:36:32 +0530

    changes are the recap

--- TreeSetDemo.java
+++ TreeSetDemo.java
@@ -11,5 +11,20 @@ public  class TreeSetDemo{
         sweets.add("barfi");
 
         System.out.println("sweets are :" +sweets);
+
+        System.out.println("contains:" +sweets.contains("basundi"));
+        System.out.println("sweets are:" +sweets);
+
+        // decendingSet, iterator,clear,getfrist,getlast,
+
+        System.out.println("size :" +sweets.size());
+        System.out.println("floor :" +sweets.floor("gajar halwa") );
+        System.out.println("fist :" +sweets.first());
+        System.out.println("lower :" +sweets.lower("rasmalai"));
+        System.out.println("higher :" +sweets.higher("bundi"));
+
+        System.out.println(sweets.descendingSet());
+
+
     }
 }

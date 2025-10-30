13f0d88cd0480a2e1068d693bdfdff10cee2640b
nique-e <niteshmule023@gmail.com>
2025-10-30 15:25:09 +0530

    Tree Set sol problem

---
+++ TreeSetDemo.java
@@ -0,0 +1,15 @@
+
+import java.util.TreeSet;
+
+public  class TreeSetDemo{
+    public static void main(String[] args) {
+        
+        TreeSet<String> sweets = new TreeSet<String>();
+        sweets.add("jaledi");
+        sweets.add("ladu");
+        sweets.add("pedha");
+        sweets.add("barfi");
+
+        System.out.println("sweets are :" +sweets);
+    }
+}
